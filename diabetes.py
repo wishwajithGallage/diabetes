cf65a11e032d4850aa9474ad9566e7b779199063
wishwajithGallage <112330401+wishwajithGallage@users.noreply.github.com>
2024-02-02 19:22:35 +0530

    diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -8,7 +8,7 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('E:\ml projects\Classification Healthcare Projects using Machine Learning 4\multiple desease prediction\Multiple Disease Prediction System\saved models\diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
 
 
